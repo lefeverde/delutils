4eb7de09143d8682bb89b6065a5f0c1f4d7dd7f1
Daniel Lefever <lefeverde@gmail.com>
2021-06-08 11:09:37 -0400

    re-organized code

--- R/misc_utils.R
+++ R/misc_utils.R
@@ -66,3 +66,210 @@ find_na_rows <- function(df){
   return(weird_rows)
 }
 
+#' Uniquefy by variance
+#'
+#' Uniquefies an matrix by the row variance. This
+#' ranks variance for each as specifified in the
+#' map data.frame. The row.names of the expression
+#' matrix and map need to have at least some in common.
+#' Function first identifies which row.names are shared
+#' and then subsets.
+#' Idea for using max var to de-duplicate probes came
+#' form here: https://www.biostars.org/p/51756/#51875
+#'
+#'
+#' @param mat expression matrix
+#' @param map gene map
+#' @param dup_col col with potential duplicates
+#'
+#' @return filtered expression matrix
+#' @export
+#'
+#' @examples
+uniquefy_by_variance <- function(mat, map, dup_col=NULL){
+
+  if(!any(class(mat) %in% c('data.frame','matrix'))){
+    stop('expr_mat needs to be either a data.frame or matrix of gene expression value.')
+  }
+
+  if(is.null(dup_col)){
+    dup_col <- names(map)[1]
+    warning(paste0('Using map ', dup_col, ' as dup_col'))
+
+  }
+
+  if(!any(row.names(mat) %in% row.names(map))){
+    stop('None of the row.names matched between mat and map.')
+  }
+  # Sorts and keeps only rows shared between map and mat
+  common_ids <- intersect(row.names(mat), row.names(map))
+  mat <- mat[common_ids,]
+  map <- map[common_ids,dup_col]
+
+  # Gets row vars and filters
+  rv_mat <- data.frame(row.names = common_ids,
+                       group=map,
+                       vals=matrixStats::rowVars(mat))
+  rv_mat <- uniquefy_base(rv_mat, 'group', 'vals')
+  filt_mat <- mat[rv_mat,]
+  return(filt_mat)
+
+}
+
+
+#' Return a unique data.frame by max value
+#'
+#' @param dat data.frame with 1st column as string and second as numeric
+#' @param group_col string denoting group column (col with duplicates)
+#' @param val_col string denoting value column
+#'
+#' @return unique data.frame
+#' @export
+#'
+#' @examples
+#' set.seed(42)
+#' dat <- data.frame(group=c('a', 'a', 'a', 'b', 'c', 'c'), vals=runif(6))
+#' dat$vals[6] <- -4
+uniquefy_by_abs_max <- function(dat, group_col=NULL, val_col=NULL){
+  #TODO make this workhorse functions which
+  # and split the variance and other uniquefiers
+  # into compatible functions
+  if(is.null(group_col)){
+    group_col <- names(dat)[1]
+  }
+  if(is.null(val_col)){
+    val_col <- names(dat)[2]
+  }
+  temp_dat <- data.frame(dat)
+  temp_dat[val_col] <- abs(temp_dat[val_col])
+  rows_to_keep <- uniquefy_base(temp_dat, group_col, val_col)
+  dat <- dat[rows_to_keep,]
+  return(dat)
+}
+
+
+
+
+
+#' Base function which does the uniquefying
+#'
+#' This is meant to be an internal function
+#' which is the commonly used across the
+#' different uniquefiers. It works by
+#' taking uniquefying based on largest value
+#' and returning a logical vector
+#'
+#' @param dat data frame
+#' @param group_col group_col string denoting group column (col with duplicates)
+#' @param val_col string denoting value column
+#'
+#' @return logical vector
+#' @keywords internal
+#'
+#' @examples
+uniquefy_base <- function(dat, group_col, val_col){
+
+  ordered_dat <- dat[order(dat[group_col], -dat[val_col]),]
+  ordered_dat <- ordered_dat[!duplicated(ordered_dat[group_col]),]
+  logical_idxs <- row.names(dat) %in% row.names(ordered_dat)
+
+  return(logical_idxs)
+}
+
+#' rbinds a named list of data.frames
+#'
+#'
+#' rbinds together data.frames in which the list name
+#' is turned into a column and bound to the data.frame
+#' becoming the first column of the data.frame
+#'
+#' @param df_list named list of data.frames
+#' @param sum_col_name colname of new column bound to data.frame
+#'
+#' @return a single data.frame row bound data.frame
+#' @export
+#'
+#' @examples
+#' library(dplyr)
+#' library(delutils)
+#' l <- list(data.frame(A=seq(1,5)), data.frame(A=seq(6, 7))) %>%
+#'  setNames(., c('Cond1', 'Cond2'))
+#'  rbind_named_df_list(l)
+#'
+#' # Doesn't work because list contains matrix
+#' \dontrun{
+#' l <- list(data.frame(A=seq(1,5)),
+#'  data.frame(A=seq(6, 7)),
+#'  matrix(data=NA, ncol=1, nrow=5)) %>%
+#'  setNames(., c('Cond1', 'Cond2', 'Cond3'))
+#'  rbind_named_df_list(l)
+#' }
+rbind_named_df_list <- function(df_list, sum_col_name='col_from_list'){
+  # Makes sure list has names, everything is
+  # either a data.frame or NULL
+  nms <- names(df_list)
+  if(is.null(nms)){
+    stop('df_list needs to be a named list')
+  }
+  item_class_checker <-
+    vapply(df_list, function(x){
+      any(class(x) %in% c('data.frame', 'NULL'))
+    }, logical(1)) %>%
+    all(.)
+  if(! item_class_checker){
+    stop('df_list contains items which are not data.frame or tibble')
+  }
+
+
+
+  out_df <- list()
+  empty_dfs <- NULL
+  for(x in nms){
+    temp_pth <- df_list[[x]]
+    valid_conditions <- c(
+      nrow(temp_pth) >= 1, # more than 1 rows
+      !is.null(temp_pth) # df is not NULL
+    ) %>% all
+    # Check valid conditions
+    if(valid_conditions){
+      temp_pth <- cbind(x,
+                        temp_pth,
+                        stringsAsFactors = FALSE)
+      row.names(temp_pth) <- NULL
+      names(temp_pth)[1] <- sum_col_name
+      out_df[[x]] <- temp_pth
+    } else {
+      empty_dfs <- c(empty_dfs, x)
+      next
+    }
+  }
+  if(!is.null(empty_dfs)){
+    warning_str <-
+      paste0(head(empty_dfs), collapse ='\n') %>%
+      paste0('Some items contained empty or otherwise disagreeable data.frames and were not included in the bound data.frame.\nThese include:\n', . )
+    warning(warning_str, sep='')
+  }
+
+  n_cols <- lapply(out_df, function(x){
+    colnames(x) %>%
+      length
+  }) %>% do.call(c, .)
+
+  if(length(unique(n_cols)) > 1){
+    suspect_dfs <-  names(out_df)[which(n_cols != mode(n_cols))]
+    e_message <-
+      paste0(suspect_dfs, collapse='\n') %>%
+
+      paste0('data.frames have different number of columns. check:\n', . )
+    stop(e_message, sep='')
+  }
+
+  # TODO wrap this trycatch when data.frames have
+  # different columns, s.t., an error w/ offending
+  # items is spit out
+  out_df <- do.call(rbind, c(out_df, stringsAsFactors=FALSE))
+  row.names(out_df) <- NULL
+  return(out_df)
+}
+
+
--- tests/testthat/test_misc_utils.R
+++ tests/testthat/test_misc_utils.R
@@ -1,5 +1,18 @@
-context("misc_utils")
+
 library(delutils)
+context("misc_utils")
+
+create_example_dataframe <- function(){
+  set.seed(42)
+  mat <- data.frame(group=c('a', 'a', 'a', 'b', 'c', 'c'), vals=runif(6) * 100)
+  mat$vals[6] <- -400
+  mat$vals <- round(mat$vals)
+  row.names(mat) <- paste0('r_', seq(1,6))
+  return(mat)
+}
+
+
+#### clean_by_levels ####
 
 test_that('clean_by_levels correctly checks input for correct type', {
   expect_error(clean_by_levels(dat=matrix()), 'dat needs to be a data.frame not a matrix')
@@ -13,4 +26,90 @@ test_that('clean_by_levels cleans a data.frame', {
   ex_dat <- data.frame(B=seq(1,4), C=c('f1', 'f2', 'f1', 'f1'))
   expect_equal(clean_by_levels(dat=in_dat), ex_dat)
 
+})
+
+
+#### rbind_named_df_list ####
+test_that('rbind_named_df_list returns a bound data.frame',{
+  el <- data.frame(
+    col_from_list=paste0('Cond',
+                         c(rep(2, 5), rep(3,2))),A=seq(1,7))
+
+  l <- list(data.frame(A=seq(1,5)),
+            data.frame(A=seq(6, 7))) %>%
+    setNames(., c('Cond2', 'Cond3'))
+  expect_equal(rbind_named_df_list(l), el)
+  # Making sure it works with tibbles
+  l2 <- lapply(l, tibble::as_tibble)
+  expect_equal(rbind_named_df_list(l2), el)
+})
+
+test_that('rbind_named_df_list checks for non data.frames', {
+  l <- list(matrix(data=NA, ncol=1, nrow=5),
+            data.frame(A=seq(1,5)),
+            data.frame(A=seq(6, 7))) %>%
+    setNames(., c('Cond1', 'Cond2', 'Cond3'))
+  expect_error(rbind_named_df_list(l),"df_list contains items which are not data.frame or tibble" )
+})
+
+
+
+test_that('rbind_named_df_list can (gracefully) handle empty data.frames or NULL objects',{
+  el <- data.frame(col_from_list=paste0('Cond', c(rep(2, 5), rep(3,2))),A=seq(1,7))
+  l_empty <- list(data.frame(),
+                  data.frame(A=seq(1,5)),
+                  data.frame(A=seq(6, 7))) %>%
+    setNames(., c('Cond1', 'Cond2', 'Cond3'))
+  l_null <- list(NULL,
+                 data.frame(A=seq(1,5)),
+                 data.frame(A=seq(6, 7))) %>%
+    setNames(., c('Cond1', 'Cond2', 'Cond3'))
+  expect_equal(expect_warning(rbind_named_df_list(l_empty), 'Cond1'), el)
+  expect_equal(expect_warning(rbind_named_df_list(l_null), 'Cond1'), el)
+})
+
+test_that('rbind_named_df_list complains loudly about data.frames with mismatched columns',{
+  l <- list(data.frame(A=seq(1,5), B=seq(8,12)),
+            data.frame(A=seq(6, 7))) %>%
+    setNames(., c('Cond2', 'Cond3'))
+  expect_error(rbind_named_df_list(l))
+})
+
+#### uniquefy ####
+
+test_that('uniquefy (base) function returns a logical vector of uniquefied rows by largest value data.frame',{
+  mat <-  create_example_dataframe()
+  e_vec <- c(FALSE,TRUE,FALSE,TRUE,TRUE,FALSE)
+  out_vec <- uniquefy_base(mat, 'group', 'vals')
+  expect_equal(out_vec, e_vec)
+
+})
+
+test_that('uniquefy by abs max returns a uniquefied (by abs(max)) data.frame',{
+  mat <-  create_example_dataframe()
+  e_mat <- data.frame(group=c('a', 'b', 'c'),
+                      vals=c(94,83,-400),
+                      row.names = c('r_2','r_4','r_6'))
+  out_mat <- uniquefy_by_abs_max(mat)
+
+  expect_equal(row.names(out_mat), row.names(e_mat))
+  expect_equal(out_mat$group, e_mat$group)
+  expect_equal(out_mat$vals, e_mat$vals)
+})
+
+test_that('uniquefy by variance returns a uniquefied (by variance) data.frame of expression data',{
+
+
+  map <- data.frame(row.names=paste0('gene', seq(1,6)),
+                    duplicated_ids=c('a','a','b','b','b','c'))
+  gene_vars <- c(2, 1, .5, 1, 4, 1)
+  set.seed(42)
+  mat <- lapply(gene_vars, function(x){
+    rnorm(10, 0, x)
+  }) %>% do.call(rbind, .)
+  row.names(mat) <- row.names(map)
+  e_vec <-  c(TRUE,FALSE,FALSE,FALSE,TRUE,TRUE)
+  e_mat <- mat[e_vec,]
+  out_mat <- uniquefy_by_variance(mat, map, 'duplicated_ids')
+  expect_equal(out_mat, e_mat)
 })
